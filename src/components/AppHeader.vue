4460cbe919b9b15bd238d72813e00b02bad73c7f
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-30 11:58:43 +0200

    Header styled

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,14 +11,14 @@
 
 <template>
     <header>
-        <div class="container">
-            <div class="row">
+        <div class="container-fluid">
+            <div class="row text-center">
                 <div class="col-2 p-2 m-2">
                     <img src="../assets/dc-logo.png" alt="Logo">
                 </div>
-                <div class="col-9 p-2 m-2 d-flex align-items-center ml-auto">
+                <div class="col-9 p-2 m-2 d-flex align-items-center justify-content-end">
                     <nav>
-                        <ul class="nav">
+                        <ul class="nav fw-bold">
                             <li v-for="(item, index) in menuItems" :key="index" class="nav-item">
                             <a class="nav-link" href="#">{{ item }}</a>
                             </li>
@@ -31,5 +31,32 @@
 </template>
 
 <style lang="scss" scoped>
+@import './styles/generals.scss';
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.row {
+    justify-content: space-between;
+}
+
+.nav-link {
+    color: $text-dark;
+    text-decoration: none;
+    font-size: 0.8rem;
+
+    &:hover {
+        text-decoration: underline;
+    }
+
+    &:visited,
+    &:link,
+    &:active {
+    color: #4d4d4d;
+    }
+}
 
 </style>
