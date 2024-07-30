a506067eeacc4d8a393211d2d4cb7e08f4b49e39
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-30 11:31:18 +0200

    Header added using bootstrap classes + v-for to cycle nav array

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,7 +10,24 @@
 </script>
 
 <template>
-
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-2 p-2 m-2">
+                    <img src="../assets/dc-logo.png" alt="Logo">
+                </div>
+                <div class="col-9 p-2 m-2 d-flex align-items-center ml-auto">
+                    <nav>
+                        <ul class="nav">
+                            <li v-for="(item, index) in menuItems" :key="index" class="nav-item">
+                            <a class="nav-link" href="#">{{ item }}</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
