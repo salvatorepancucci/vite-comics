d85e229c76b7b64ccdbfce305604106b7659d978
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-30 16:31:39 +0200

    Minor fixes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,59 +1,59 @@
 <template>
-    <div class="col-2 mb-3">
-      <div class="card">
-        <img :src="thumb" class="card-img-top" :alt="series">
-        <div class="card-body">
-          <h5 class="card-title">{{ series }}</h5>
-          <p class="card-text">{{ type }}</p>
-          <p class="card-text">{{ price }}</p>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2 mb-3 d-flex">
+        <div class="card h-100">
+          <img :src="thumb" class="card-img-top" :alt="series">
+          <div class="card-body">
+            <h5 class="card-title">{{ series }}</h5>
+            <p class="card-text">{{ type }}</p>
+          </div>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Card',
-    props: {
-      thumb: String,
-      price: String,
-      series: String,
-      type: String
-    }
-  }
-  </script>
-  
-  <style lang="scss" scoped>
-  @import './styles/generals.scss';
+  </div>
+</template>
 
-  .card {
-    border: none;
-    width: 150px; 
-    height: 250px;
-    background-color: $secondary-color;
-    color: white;
+<script>
+export default {
+  name: 'Card',
+  props: {
+    thumb: String,
+    price: String,
+    series: String,
+    type: String
   }
+}
+</script>
 
-  .card-img-top {
-    height: 150px; 
-    width: 150px;
-    object-fit: cover;
-    margin-bottom: 5px;
-  }
+<style lang="scss" scoped>
+@import './styles/generals.scss';
 
-  .card-body {
-    padding: 2px;
-  }
+.card {
+  border: none;
+  width: 100%;
+  background-color: $secondary-color;
+  color: white;
+}
 
-  .card-title {
-    font-size: 0.875rem; 
-    font-weight: bold;
-  }
+.card-img-top {
+  height: 150px;
+  width: 100%; 
+  object-fit: cover;
+  margin-bottom: 5px;
+}
 
-  .card-text {
-    font-size: 0.75rem; 
-    margin-bottom: 0.25rem;
-  }
+.card-body {
+  padding: 10px;
+}
+
+.card-title {
+  font-size: 0.875rem;
+  font-weight: bold;
+}
+
+.card-text {
+  font-size: 0.75rem;
+  margin-bottom: 0.25rem;
+}
 </style>
-
-  
