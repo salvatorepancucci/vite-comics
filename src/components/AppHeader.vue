569498d72b6a858b8d8ee7e9824c64ba9b875c7c
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-30 11:18:11 +0200

    String array for nav item added to script on AppHeader.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        name: 'AppHeader',
+        data() {
+            return {
+                menuItems: ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP']
+            }
+        }
+    }
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
